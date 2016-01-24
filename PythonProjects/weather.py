0ca2d97ac62cd977063af42c111af0617ed4c2ff
Markus Vieth <mavieth@students.uni-mainz.de>
2016-01-25 00:04:47 +0100

    workaround

--- PythonProjects/weather.py
+++ PythonProjects/weather.py
@@ -12,7 +12,23 @@ owm = OWM(API_key=API_KEY, language='de')
 mainzId = 2874225
 
 #mainzWeather = owm.weather_at_id(mainzId)
-mainzFc = owm.three_hours_forecast(mainzId, limit=3)
+mainzFc = owm.three_hours_forecast_at_id(mainzId)
+mainzF = mainzFc.get_forecast()
+weatherList = mainzF.get_weathers()
+threeHours = 3*60*60
+newWeatherList = []
+time = mainzFc.when_starts()
+interval = mainzF.get_interval()
+reception_time = mainzF.get_reception_time(timeformat='unix')
+location = mainzF.get_location()
+
+
+for i in range(0,2):
+	newWeatherList.append(mainzFc.get_weather_at(time+threeHours*i))
+
+MainzF = pyowm.webapi25.forecast.Forecast(interval,reception_time,location,newWeatherList)
+MainzFc = pyowm.webapi25.forecaster.Forecaster(MainzF)
+
 
 
 
